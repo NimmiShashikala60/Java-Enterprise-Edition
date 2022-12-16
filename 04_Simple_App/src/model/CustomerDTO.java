2dbca78bef73e0836d2e1eafd69d2f6a535ff3dc
NimmiShashikala60 <nimmishashikala385@gmail.com>
2022-12-16 23:19:32 +0530

    new update

--- 04_Simple_App/src/model/CustomerDTO.java
+++ 04_Simple_App/src/model/CustomerDTO.java
@@ -1,4 +1,15 @@
 package model;
 
+import lombok.*;
+
+
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Data
 public class CustomerDTO {
+    String id;
+    String name;
+    String address;
+    int salary;
 }
